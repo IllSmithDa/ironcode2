0b76d477dca9992e56d77e16759afd09493d8d43
Samuel <sam404kim@gmail.com>
2023-12-11 11:10:04 -0800

    added compare page basic content

---
+++ src/app/compare_two/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CompareTwo.scss';
+
+export default function CompareTwo() {
+
+  // use local storage to save the values of two languages to compare with each other
+  return (
+    <div className='compare=page'>
+      <h1>Site In Progress...</h1>
+    </div>
+  )
+}
