c0d53e20bf2b30f489bd07b0856835cbd565c849
Samuel <sam404kim@gmail.com>
2023-12-13 18:10:45 -0800

    added html symbol

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -140,7 +140,7 @@ export default function Navbar({
           </li>
           <li className={`navbar-dropdown list-right ${darkMode ? 'dark': ''}`}>
             <button type='button' onClick={() => setLanguageDropdown(!languageDropdown)} className='menu-tabs'>
-              {languageId ? languageOption : `Select`} {languageDropdown ? <>&#11205;</> : <>&#11206;</>}
+              {languageId ? languageOption : `Select`} {languageDropdown ? <>▼</> : <>▼</>}
             </button>
           </li>
           <li  className='app-icons'>
