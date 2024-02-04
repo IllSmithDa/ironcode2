27932bb920244dcb54ef7951f2c9646620efba45
Samuel <sam404kim@gmail.com>
2024-02-03 18:38:04 -0800

    updated parser

--- src/app/helper/parseData.ts
+++ src/app/helper/parseData.ts
@@ -23,6 +23,7 @@ export const parseConcepts = (htmlString: string, elementId: string) => {
   newText = newText?.replace(/\/\*\nmulti \nline \ncomment\n\*\//g, `<font color="#22AA22">${`\/\*\nmulti \nline \ncomment\n\*\/`}</font>`) as string;
   newText = newText?.replace(/\"\"\"\nmulti \nline \ncomment\n\"\"\"/g, `<font color="#22AA22">${`\"\"\"\nmulti \nline \ncomment\n\"\"\"`}</font>`) as string;
   newText = newText?.replace(/\=begin\nmulti \nline \ncomment\n\=end/g, `<font color="#22AA22">${`\=begin\nmulti \nline \ncomment\n\=end`}</font>`) as string;
+  newText = newText?.replace(/\%\{\nmulti \nline \ncomment\n\%\}/g, `<font color="#22AA22">${`\%\{\nmulti \nline \ncomment\n\%\}`}</font>`) as string;
   // comment matching
   let doubleQuotes = newText?.match(/["'][a-zA-Z0-9\\\n-\s\/!_\:\,\{\}\%!"]*["?'?]/g)
   doubleQuotes?.forEach((stringVal) => {
@@ -30,7 +31,7 @@ export const parseConcepts = (htmlString: string, elementId: string) => {
     newText = newText?.replace(regexp, `<font color="#E42217">${stringVal}</font>`)
   })
 
-  let comments = newText?.match(/(\/\/[a-zA-Z0-9\s]+\n)|(\#[a-zA-Z0-9\s]+\n)|(\"\"\"[a-zA-Z0-9\n\s]+\"\"\")/g);
+  let comments = newText?.match(/(\/\/[a-zA-Z0-9\s]+\n)|(\#[a-zA-Z0-9\s]+\n)|(\"\"\"[a-zA-Z0-9\n\s]+\"\"\")|(\%[a-zA-Z0-9\s]+\n)/g);
   comments?.forEach((stringVal) => {
     var regexp = new RegExp(stringVal, "g");
     newText = newText?.replace(regexp, `<font color="#22AA22">${stringVal}</font>`);
